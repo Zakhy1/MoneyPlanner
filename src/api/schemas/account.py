9f829185cc2847a4af73b7525fd9eca8c1000285
zakhy <z.lyrshikov@cybercode.pro>
2026-06-05 15:42:24 +0700

    !

---
+++ src/api/schemas/account.py
@@ -0,0 +1,10 @@
+class AccountCRUD:
+    pass
+
+
+class AccountPartialUpdate:
+    pass
+
+
+class AccountPublic:
+    pass
--- src/api/v1/account.py
+++ src/api/v1/account.py
@@ -1,4 +1,92 @@
-from fastapi import APIRouter
+import uuid
+from typing import Annotated
 
+from fastapi import APIRouter, Depends, Query
+from sqlmodel.ext.asyncio.session import AsyncSession
+from starlette import status
+
+from api.schemas.account import AccountPartialUpdate, AccountPublic
+from core.dependencies.user import get_current_user
+from db.postgres import get_session
+from models import Category
+from services.account import AccountService
 
 router = APIRouter()
+
+
+async def get_account_service(
+    session: Annotated[AsyncSession, Depends(get_session)],
+):
+    return AccountService(session)
+
+
+@router.get(
+    "/",
+)
+async def list_account(
+    current_user: Annotated[dict, Depends(get_current_user)],
+    service: Annotated[AccountService, Depends(get_account_service)],
+    page: Annotated[int, Query(ge=1)] = 1,
+    page_size: Annotated[int, Query(ge=1, le=100)] = 10,
+) -> list[AccountPublic]:
+    data = await service.get_list_account(
+        page=page, page_size=page_size, user_id=current_user["id"]
+    )
+    return [AccountPublic.model_validate(account) for account in data]
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_account(
+    payload: AccountPublic,
+    current_user: Annotated[dict, Depends(get_current_user)],
+    service: Annotated[AccountService, Depends(get_account_service)],
+) -> AccountPublic:
+    data = await service.create_account(
+        Category(**payload.model_dump() | {"user_id": current_user["id"]})
+    )
+    return AccountPublic.model_validate(data)
+
+
+@router.put(
+    "/{account_id}",
+)
+async def update_account(
+    account_id: uuid.UUID,
+    payload: AccountPublic,
+    current_user: Annotated[dict, Depends(get_current_user)],
+    service: Annotated[AccountService, Depends(get_account_service)],
+) -> AccountPublic:
+    data = await service.update_account(
+        account_id,
+        Category(
+            **payload.model_dump() | {"user_id": current_user["id"]} | {"id": account_id}
+        ),
+        current_user["id"],
+    )
+    return AccountPublic.model_validate(data)
+
+
+@router.patch(
+    "/{account_id}",
+)
+async def partial_update_account(
+    account_id: uuid.UUID,
+    payload: AccountPartialUpdate,
+    current_user: Annotated[dict, Depends(get_current_user)],
+    service: Annotated[AccountService, Depends(get_account_service)],
+) -> AccountPublic:
+    data = await service.partial_update_account(
+        account_id,
+        current_user["id"],
+        payload.model_dump(exclude_unset=True),
+    )
+    return AccountPublic.model_validate(data)
+
+
+@router.delete("/{account_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_account(
+    account_id: uuid.UUID,
+    current_user: Annotated[dict, Depends(get_current_user)],
+    service: Annotated[AccountService, Depends(get_account_service)],
+):
+    await service.delete_account(account_id, current_user["id"])
