a7e7e2d78c0d35dce62569e5ef13c3e60c665104
zakhy <z.lyrshikov@cybercode.pro>
2026-06-06 21:58:50 +0700

    ! add blanc methods

--- src/services/account.py
+++ src/services/account.py
@@ -1,2 +1,21 @@
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+
 class AccountService:
-    pass
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_list_account(self, page, page_size, user_id):
+        pass
+
+    async def create_account(self, param):
+        pass
+
+    async def update_account(self, account_id, param, param1):
+        pass
+
+    async def partial_update_account(self, account_id, param, param1):
+        pass
+
+    async def delete_account(self, account_id, param):
+        pass
